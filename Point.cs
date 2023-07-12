15329304154b9cb775eb41fdefa7791350574c12
yasha <yasha@LENOVOV15>
2023-07-12 16:48:13 +0400

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public struct Point
+    {
+        private float x = 0;
+        private float y = 0;
+        private bool boolean = true;
+        public Point(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point(bool b)
+        {
+            this.boolean = b;
+        }
+        public static Point operator +(Point p1,Point p2)
+        {
+            return new Point(p1.x + p2.x, p1.y + p2.y);
+        }
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.x - p2.x, p1.y - p2.y);
+        }
+        public static Point operator /(Point p1, Point p2)
+        {
+           if(p1.y ==0 || p2.y==0) throw new DivideByZeroException();
+            return new Point(p1.x / p2.x, p1.y / p2.y);
+        }
+        public static Point operator *(Point p1, Point p2)
+        {
+            return new Point(p1.x * p2.x, p1.y * p2.y);
+        }
+        public static bool operator >=(Point p1,Point p2)
+        {
+            if (p1.x >= p2.x && p1.y >= p2.y) return true;
+            return false;
+        }
+        public static bool operator <=(Point p1, Point p2)
+        {
+            if (p1.x <= p2.x && p1.y <= p2.y) return true;
+            return false;
+        }
+        public static Point operator &(Point p1,Point p2)
+        {
+            if (p1.boolean && p2.boolean) return new Point(true);
+            return new Point(false);
+        }
+        public static Point operator |(Point p1, Point p2)
+        {
+            if (p1.boolean || p2.boolean) return new Point(true);
+            return new Point(false);
+        }
+        public void GetPoint()
+        {
+            Console.WriteLine($"x = {x}\ny = {y}\n--------------------");
+        }
+        public void GetBool()
+        {
+            Console.WriteLine($"Bool is {boolean}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using OperatorOverloading;
+
+var p1 = new Point(10, 20);
+var p2 = new Point(5, 4);
+
+var pSum = p1 + p2;
+pSum.GetPoint();
+
+var pSub = p1 - p2;
+pSub.GetPoint();
+
+var pDiv = p1 / p2;
+pDiv.GetPoint();
+
+var pMul = p1 * p2;
+pMul.GetPoint();
+
+Console.WriteLine(p1>=p2);
+Console.WriteLine(p1<=p2);
+Console.WriteLine("-------------");
+
+var p3 = new Point(true);
+var p4 = new Point(false);
+
+var pAnd = p3 & p4;
+pAnd.GetBool();
+
+var pOr = p3 | p4;
+pOr.GetBool();
+
+
